3a8ce625a616b0910d2fb37640e7f9b8a1d9369a
telleQ <STEP.nst@gmail.com>
2020-10-04 22:48:50 +0300

    feat: add functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
+exports.min = function min(array) {
+    if (array === [] || Array.isArray(array) === false) {
+        return 0;
+    } else {
+        return min = Math.min.apply(null, array);
+    }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array === [] || Array.isArray(array) === false) {
+        return 0;
+    } else {
+        return max = Math.max.apply(null, array);
+    }
 
-exports.max = function max (array) {
-  return 0;
-}
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array === [] || Array.isArray(array) === false) {
+        return 0;
+    } else {
+        const reducer = (accumulator, currentValue) => accumulator + currentValue;
+        return array.reduce(reducer) / array.length;
+    }
+
+};
